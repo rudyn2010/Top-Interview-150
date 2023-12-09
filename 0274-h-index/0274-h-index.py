1b6df5a467d0f2cd3004100de94e284080e4dabd
Rudy Nguyen <rudyn2010@berkeley.edu>
2023-12-08 18:59:07 -0800

    Time: 44 ms (61.32%), Space: 16.7 MB (8.14%) - LeetHub

---
+++ 0274-h-index/0274-h-index.py
@@ -0,0 +1,8 @@
+class Solution:
+    def hIndex(self, citations: List[int]) -> int:
+        citations.sort(reverse = True)
+        
+        for idx, citation in enumerate(citations):
+            if idx >= citation:
+                return idx
+        return len(citations)
